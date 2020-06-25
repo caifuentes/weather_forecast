4833af427bd3c981fcc784126a55334194a64e3a
charmaine.fuentes <charmaine.fuentes03@gmail.com>
2020-06-25 22:33:12 +0800

    FEA-1 Added app models

---
+++ app/src/main/java/com/weather/weather_forecast/models/Details.kt
@@ -0,0 +1,25 @@
+package com.weather.weather_forecast.models
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class Details(
+    @SerializedName("weather")
+    @Expose
+    val weather: MutableList<Weather> = mutableListOf(),
+    @SerializedName("main")
+    @Expose
+    val main: Main,
+    @SerializedName("visibility")
+    @Expose
+    val visibility: Int,
+    @SerializedName("dt")
+    @Expose
+    val dt: Int,
+    @SerializedName("id")
+    @Expose
+    val id: Int,
+    @SerializedName("name")
+    @Expose
+    val name: String
+)
